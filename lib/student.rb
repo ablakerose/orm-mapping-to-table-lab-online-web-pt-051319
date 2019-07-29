000f957442dce451d790bceb5516bc83a97ae593
Blake Rose <bdecktor@gmail.com>
2019-07-29 23:30:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,9 +33,9 @@ class Student
   end
 
   def self.create(name:, grade:)
-   song = Student.new(name, grade)
-   song.save
-   song
+   student = Student.new(name, grade)
+   student.save
+   student
  end
 
 end
