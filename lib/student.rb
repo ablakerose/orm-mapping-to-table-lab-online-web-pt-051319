5f378e7e056e4ee49c3e7e36e0211e64624ff9d1
Blake Rose <bdecktor@gmail.com>
2019-07-29 23:20:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,22 +4,22 @@ class Student
   #  with DB[:conn]
 
   attr_accessor :name, :grade, :id
- 
+
   def initialize(name, album, id=nil)
     @id = id
     @name = name
     @album = album
   end
- 
+
   def self.create_table
-    sql =  <<-SQL 
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
         album TEXT
         )
         SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
 
 
